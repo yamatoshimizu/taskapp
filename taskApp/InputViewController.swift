29e920022c8de5b2b0885dfaf7b47682af25243a
Yamato Shimizu <yamato.shimiu.0629@gmail.com>
2020-07-09 00:03:42 +0900

    三画面作ってRealmもインストールした。

---
+++ taskApp/InputViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InputViewController.swift
+//  taskApp
+//
+//  Created by 清水大和 on 2020/07/08.
+//  Copyright © 2020 Yamato Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var ContentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
