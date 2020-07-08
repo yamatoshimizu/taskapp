3c29793967092315f1481c393defb97d0b285378
Yamato Shimizu <yamato.shimiu.0629@gmail.com>
2020-07-09 01:05:09 +0900

    バグった

--- taskApp/InputViewController.swift
+++ taskApp/InputViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var ContentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+    let realm = try! Realm()
+    var task: Task!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        ContentsTextView.text = task.content
+        datePicker.date = task.date
+    }
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write{
+            self.task.title = self.titleTextField.text!
+            self.task.content = self.ContentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+        super.viewWillDisappear(animated)
+    }
 
     /*
     // MARK: - Navigation
