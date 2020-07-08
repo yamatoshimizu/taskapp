caa935244a9ccda4a4bfcbac7e87c9ea7a619858
Yamato Shimizu <yamato.shimiu.0629@gmail.com>
2020-07-08 23:24:00 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 清水大和 on 2020/07/08.
+//  Copyright © 2020 Yamato Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
